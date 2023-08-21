c48d742c0e80a20117d48002eea4d6e39432643b
sawlukbloos <sawlukbloos@gmail.com>
2023-08-21 14:39:46 -0500

    Prueba de cambio

--- Primer proyecto/src/primer/proyecto/PrimerProyecto.java
+++ Primer proyecto/src/primer/proyecto/PrimerProyecto.java
@@ -15,6 +15,7 @@ public class PrimerProyecto {
      */
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        //solo es un rpoyecto de prueba para probar neatbens, github y sourcetree
         // TODO code application logic here
     }
     
