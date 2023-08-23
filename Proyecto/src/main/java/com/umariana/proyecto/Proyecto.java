c162127e60545a96c807bf37395d0b75d2f9d35b
sawlukbloos <124732664+sawlukbloos@users.noreply.github.com>
2023-08-22 20:56:20 -0500

    Se me olvido agregar las librerías XD

--- Proyecto/src/main/java/com/umariana/proyecto/Proyecto.java
+++ Proyecto/src/main/java/com/umariana/proyecto/Proyecto.java
@@ -4,7 +4,12 @@
 
 package com.umariana.proyecto;
 import Mundo.Alumno;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
